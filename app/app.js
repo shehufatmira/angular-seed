4ef6517383b017ad95c61fb750894d388da52f67
shehufatmira <fatmira.shehu@hotmail.com>
2018-01-25 12:24:49 +0100

    updated Project Tree

--- app/app.js
+++ app/app.js
@@ -1,14 +1,5 @@
-'use strict';
+(funtion()){
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var app = angular.module('myBooks', []);     //ketu bejme inicizlizimin e angularit
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+})();
